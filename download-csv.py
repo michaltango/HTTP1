c0f96e99e6675dc984bd34e0df8c3a9e0370e34e
MichalTango <trompele@gmail.com>
2017-07-16 15:11:06 +0100

    http progress

--- download-csv.py
+++ download-csv.py
@@ -1,9 +1,29 @@
 import urllib.request
-url = "https://github.com/michaltango/Python1/blob/master/file1.csv"
+url = "http://172.16.3.1:8080/file1.csv"
 
-csvfilename = url.split('/')[-1]
+header_in_csv = True
+position = list()
+csvfilename = url.split('/')[-1] + ".csv"
 response = urllib.request.urlopen(url)
 
-print (csvfilename)
+file = open("csvfilename",'wb')
+content = response.read()
+file.write(content)
 
+file.close()
+
+
+file = open("csvfilename",'r')
+
+if header_in_csv:
+    
+
+
+for line in file:
+     print(line)
+
+
+
+#for line in content:
+#    print(line)
 
