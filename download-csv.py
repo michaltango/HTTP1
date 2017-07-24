d460ea461fe089c3c001483723ac92cb40095d62
MichalTango <trompele@gmail.com>
2017-07-24 21:15:27 +0100

    unknown

--- download-csv.py
+++ download-csv.py
@@ -1,37 +1,61 @@
 import urllib.request
+from urllib.error import URLError, HTTPError
+import csv, os.path
 url = "http://172.16.3.1:8080/file1.csv"
-
 header_in_csv = True
-position = list()
-csvfilename = url.split('/')[-1] + ".csv"
-response = urllib.request.urlopen(url)
-print(response.info())
-csvfile_size = response.getheader("Content-Length")
-print(csvfile_size)
-csvfile_buffer = int(csvfile_size) % 10 + 1
 
 
-file = open("csvfilename",'wb')
-content = response.read(csvfile_buffer)
-file.write(content)
+#Download file from given url and save to current directory 
+def download_csv(url):
+    position = list()
+    csvfilename = url.split('/')[-1]
 
-file.close()
+    #Try to open URL, return error codes
+    try:
+        response = urllib.request.urlopen(url)
+    except HTTPError as e:
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
+        return "Error accessing file."
+    except URLError as e:
+        print('We failed to reach a server.')
+        print('Reason: ', e.reason)
+        return "Error connecting."
+    else:
+        print(response.info())
+        csvfile_size = response.getheader("Content-Length")
+        print("File size: " + csvfile_size)
 
+        #Define size of buffer
+        csvfile_buffer = int(csvfile_size) / 10 
+        csvfile_buffer = int(csvfile_buffer)
 
-file = open("csvfilename",'r')
+        #Open file and write content to it
+        file = open(csvfilename,'wb')
+        content = response.read(csvfile_buffer)
+        print("Writing: ",end='')
+        while content:
+            file.write(content)
+            print("#",end='')
+            content = response.read(csvfile_buffer)
+        file.close()
 
-if header_in_csv:
-   headerline = file.readline()
-   headers = headerline.split(',')
+    return csvfile_size
 
-for head in headers:
-    print("##" + head + "##")
-    
-for line in file:
-    line = line.rstrip()
-    print(line)
+def displaycsv(filename):
 
-file.close()
+    with open(filename,mode='r') as csvfile:
+        file = csv.DictReader(csvfile)
+
+        for row in file:
+            ipaddr = row['IP']
+            name = row['name']
+            port = row['port']
+            print(ipaddr)
+    return 1
+
+print(download_csv(url))
+print(displaycsv("file1.csv"))
 
 #for line in content:
 #    print(line)
